3fb024327d367ef9c867fb8f2a880f33ccd4b431
otoniel sena <otonielsenamendez@gmail.com>
2021-06-22 06:43:56 -0300

    version 2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using AppMastermind.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<CodePeg> listCodePeg = new List<CodePeg>();
+            listCodePeg.Add(CodePeg.Blue);
+            listCodePeg.Add(CodePeg.Green);
+            listCodePeg.Add(CodePeg.Red);
+            listCodePeg.Add(CodePeg.Yellow);
+
+            Mastermind masterMind = new Mastermind(listCodePeg);
+
+
+
+            List<CodePeg> listGess = new List<CodePeg>();
+            listGess.Add(CodePeg.Black);
+            listGess.Add(CodePeg.Yellow);
+            listGess.Add(CodePeg.Red);
+            listGess.Add(CodePeg.Blue);
+
+            List<ResultPeg> listResultPeg = masterMind.GetHints(listGess);
+
+            for (int i = 0; i < listResultPeg.Count; i++)
+            {
+                Console.Write("{0}", listResultPeg[i].ToString());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Resources/Mastermind.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMastermind.Resources
+{
+    class Mastermind
+    {
+        private readonly List<CodePeg> code;
+
+        public Mastermind(List<CodePeg> code)
+        {
+            this.code = code;
+        }
+
+        public List<ResultPeg> GetHints(List<CodePeg> guess)
+        {
+            List<ResultPeg> listResult = new List<ResultPeg>();
+
+            for (int i = 0; i < guess.Count; i++)
+            {
+                for (int e = 0; e < code.Count; e++)
+                {
+                    if (guess[i].Equals(code[e]) && i.Equals(e))
+                        listResult.Add(ResultPeg.Black);
+
+                    else if (guess[i].Equals(code[e]))
+                        listResult.Add(ResultPeg.White);
+                }
+            }
+            return listResult;
+        }
+    }
+    public enum CodePeg
+    {
+        Black,
+        Blue,
+        Green,
+        Red,
+        White,
+        Yellow,
+    }
+
+    public enum ResultPeg
+    {
+        Black,
+        White,
+    }
+}
